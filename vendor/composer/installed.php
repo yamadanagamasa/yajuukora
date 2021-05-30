ae8879ade03eba8094b98ffa659e1ce1f31ad2d4
danieru <danieru565612345@gmail.com>
2021-05-30 15:21:06 +0900

    aa

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1f732ce646bf5721641032cfe2c49271b0d7b106',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1f732ce646bf5721641032cfe2c49271b0d7b106',
+    ),
+  ),
+);
