868e283664b51859189e8e7687e2e3a027f39ed6
danieru <danieru565612345@gmail.com>
2021-05-30 15:12:59 +0900

    aa

--- index.php
+++ index.php
@@ -190,7 +190,7 @@
               move_uploaded_file($_FILES['image']['tmp_name'], 'images/' . $image);//imagesディレクトリにファイル保存
               if (exif_imagetype($file)) {//画像ファイルかのチェック
                 //$command="python main.py ";サーバー用
-                $command="python3 main.py";
+                $command="python main.py";
                 $message = '画像を判定しました';
                 exec($command);
               } else {
